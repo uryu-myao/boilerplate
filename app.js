b695ff3d662949003a8eb1cb9107e5f94f1af56c
uryu_myao <myao.jpn@gmail.com>
2022-09-12 23:34:44 +0900

    add dotenv

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 /**
+ * Set env by dotenv
+ * @see https://www.npmjs.com/package/dotenv
+ */
+require('dotenv').config()
+console.log(process.env.PRISMIC_ENDPOINT, process.env.PRISMIC_CLIENT_ID)
+
+/**
  * Express configuration
  * @see https://expressjs.com/en/4x/api.html
  */
-
 const express = require('express')
 const app = express()
 const path = require('path')
 const port = 5000
 
+/**
+ * Prismic
+ * @see https://prismic.io/docs/technologies/express-overview
+ */
 app.locals.basedir = path.join(__dirname, 'views')
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/details', (req, res) => {
+  res.render('detail')
+})
+
+app.get('/details/detail/:uid', (req, res) => {
+  res.render('details/detail')
 })
 
 app.get('/about', (req, res) => {
